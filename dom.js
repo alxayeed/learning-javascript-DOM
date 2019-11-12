589282313666a768911f72af0db4099ef828f60a
alxayeed <alxayeed@gmail.com>
2019-11-12 19:37:35 +0600

    new folder and related files are created for javascript DOM

---
+++ dom.js
@@ -0,0 +1,17 @@
+//Examine the document objects
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.title);
+// // document.title = 'changed';
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent='Changed the Title from DOM';
+
+// console.log(document.forms);
+// console.log(document.forms[0]);
+
+//SELECTORS
