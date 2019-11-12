ed3ae51448c41aedcbeab9825f6b195766babd5d
alxayeed <alxayeed@gmail.com>
2019-11-12 20:13:46 +0600

    Selectors(getgetElementById,getElementsByClasseName,getElementsByTagName

--- dom.js
+++ dom.js
@@ -15,3 +15,50 @@
 // console.log(document.forms[0]);
 
 //SELECTORS
+
+//getElementById()
+// console.log(document.getElementById('header-title'));
+var headertitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headertitle);
+// headertitle.textContent='Hello';
+// headertitle.innerText = 'Hola';
+//They are almost same,but innertext follows what is in the style
+// console.log(headertitle.textContent);
+// console.log(headertitle.innerText);
+// headertitle.innerHTML = '<h1>Hey</h1>';
+// console.log(headertitle.innerHTML);
+
+// header.style.borderBottom = 'solid 10px #000';
+
+//getElementsByClasseName()
+var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[3]);
+// items[1].textContent = 'Hello';
+// items[2].innerText = 'Hola';
+// items[3].style.fontSize='25px';
+// items[0].style.fontWeight='bold';
+// items[1].style.backgroundColor = 'orange';
+
+// items.style.backgroundColor = '#f4f4f4'; //Gives error,because items is an HTML list item
+
+// for(var i=0;i<items.length;i++){
+// 	items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+//getElementsByTagName()
+var items = document.getElementsByTagName('li');
+console.log(items);
+console.log(items[3]);
+items[1].textContent = 'Hello';
+items[2].innerText = 'Hola';
+items[3].style.fontSize='25px';
+items[0].style.fontWeight='bold';
+items[1].style.backgroundColor = 'orange';
+
+// items.style.backgroundColor = '#f4f4f4'; //Gives error,because items is an HTML list item
+
+for(var i=0;i<items.length;i++){
+	items[i].style.backgroundColor = '#f4f4f4';
+}
