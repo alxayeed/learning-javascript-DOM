9378831eb9a104515ef4b6a769710e97070849a3
alxayeed <alxayeed@gmail.com>
2019-11-13 16:03:57 +0600

    Add Items

---
+++ final.js
@@ -0,0 +1,37 @@
+var form = document.querySelector('form')
+var itemList = document.querySelector('ul')
+var filter = document.querySelector('input#filter')
+
+//Form submit event
+form.addEventListener('submit',addItem);
+
+function addItem(e){
+  e.preventDefault();
+  
+  var newItem  = document.getElementById('item').value
+
+  //new li element
+  var li  = document.createElement('li')
+
+  //add class to the li
+  li.className ='list-group-item';
+
+  //append textNode to li
+  li.appendChild(document.createTextNode(newItem))
+
+  //add delete button
+  var deleteBtn = document.createElement('button')
+
+  //add class to the button
+  deleteBtn.className = 'btn btn-danger btn-float btn-sm float-right delete'
+
+  //append textNode to li
+  deleteBtn.appendChild(document.createTextNode('x'))
+
+  //append the button in li
+  li.appendChild(deleteBtn)
+
+  itemList.appendChild(li)
+  console.log(deleteBtn)
+}
+
